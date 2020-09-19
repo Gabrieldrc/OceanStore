d8d7096b0414cea6e30f9d11f12fb942ecea4f4e
Gabriel <you@example.com>
2020-09-19 19:12:45 -0300

    adding form for new apps

--- client/src/components/NewAppForm/NewAppForm.js
+++ client/src/components/NewAppForm/NewAppForm.js
@@ -18,42 +18,29 @@ const styleCheck = {
 };
 
 function NewAppForm() {
-  const [name, setAppName] = useState('');
-  const [price, setPrice] = useState(0);
-  const [category, setCategory] = useState('');
-  const [passwordsMatch, setPasswordsMatch] = useState('');
   const [res, setRes] = useState({});
 
   function handleClick(event) {
 
     event.preventDefault();
     
-    // if (password !== password_confirm) {
-    //   setPasswordsMatch('dont match');
-    //   return;
-    // }
-
-    // setPasswordsMatch('');
-
-    // const url = '/server/users/new_user';
-    // const data = {
-    //   'user_name': user_name,
-    //   'password': password,
-    // };
-
-    // axios.post(url, data)
-    // .then(response => {
-    //   setRes({
-    //     message: response.data,
-    //     type: 'check',
-    //   });
-    // })
-    // .catch((error) => {
-    //   setRes({
-    //     message: error.response.data,
-    //     type: 'error',
-    //   });
-    // });
+    const url = '/server/apps/new_app';
+    
+    const form = document.getElementById('new_app_form');
+    const formData = new FormData(form);
+    axios.post(url, formData)
+    .then(response => {
+      setRes({
+        message: response.data,
+        type: 'check',
+      });
+    })
+    .catch((error) => {
+      setRes({
+        message: error.response.data,
+        type: 'error',
+      });
+    });
   }
 
   function labelResponse() {
@@ -72,15 +59,15 @@ function NewAppForm() {
     // Categoría.
     // Imagen o logo.
     <div>
-      <form style={styleForm} id="signupForm" enctype="multipart/form-data">
+      <form style={styleForm} id="new_app_form" encType="multipart/form-data">
         <label> Name:</label>
         <input type="text" name="app_name" onChange={event => setAppName(event.target.value)} required/>
         <label> Price:</label>
         <input type="number" name="app_price" min="0" max="100" onChange={event => setPrice(event.target.value)} required/>
         <label> Category:</label>
-        <input type="text" name="category" onChange={event => setCategory(event.target.value)} required/>  
+        <input type="text" name="app_category" onChange={event => setCategory(event.target.value)} required/>  
         <label> Image or Logo:</label>
-        <input type="file" name="image" />
+        <input type="file" name="app_image" />
         <button type="submit" onClick={event => handleClick(event)}>Submit</button>
         {labelResponse()}
       </form>
