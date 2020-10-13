f75e27761c4a64ca88c6d8673c3e77f32c47d5eb
Gabriel <you@example.com>
2020-10-13 13:45:04 -0300

    added rate controller and service

--- server/src/controllers/rates.controller.js
+++ server/src/controllers/rates.controller.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const ratesController = express.Router();
-const authJwt = require('../middleware/authJwt');
-const ac = require('../config/ac.config');
+const rateService = require('../services/rate.service');
+// const authJwt = require('../middleware/authJwt');
+// const ac = require('../config/ac.config');
+
+ratesController.get('/count/:app_name', async (req, res) => {
+  const app_name = req.params.app_name;
+
+  const appRates = await rateService.findAppRates(app_name);
+
+  return res.status(200).json(appRates);
+
+})
+
 module.exports = ratesController;
---
+++ server/src/services/rate.service.js
@@ -0,0 +1,19 @@
+const APP_RATES = require('../models').app_rates;
+
+const findAppRates = async (app_name) => {
+
+  const { count, rows } = await APP_RATES.findAndCountAll({
+    where: {
+      app_name: `${app_name}`,
+    }
+  });
+
+  return {count: count, rows: rows};
+
+};
+
+
+
+module.exports = {
+  findAppRates,
+};
