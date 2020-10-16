1899d2f0b2b5f096b088f9b50a23c2b5f2917059
Gabriel <you@example.com>
2020-10-16 18:14:17 -0300

    .

--- client/src/components/Buttom/AddCarButtom/AddCarButtom.js
+++ client/src/components/Buttom/AddCarButtom/AddCarButtom.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import style from '../Buttom.style';
 import styleAC from './AddCarButtom.style';
+import AuthService from '../../../services/auth.service';
 
 function AddCarButtom() {
   const [buttomStyle, setButtomStyle] = useState(style.getStyle(styleAC.primaryColor, styleAC.secundaryColor));
   const [content, setContent] = useState(['Add to car']);
   const handleClick = () => {
-    setContent('In car');
+    const user = AuthService.getCurrentUser();
+    if (!user) {
+    return setContent([<Redirect exact to="/signin" key="addCarNoSigned"/>]);
+    }
+    setContent([<Redirect exact to="/car" key="addCarSigned"/>]);
   };
-  const handleMouseOver = () =>{
+  const handleMouseOver = () => {
     setButtomStyle(style.getOnMouseOverStyle(styleAC.primaryColor, styleAC.secundaryColor));
   }
-  const handleMouseOut = () =>{
+  const handleMouseOut = () => {
     setButtomStyle(style.getStyle(styleAC.primaryColor, styleAC.secundaryColor));
   }
   return(
