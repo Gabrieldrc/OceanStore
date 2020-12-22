4adfe9ceacc39579c433e63d3ff88e7379641994
Gabrieldrc <gabriel.drc25@gmail.com>
2020-12-22 15:11:06 -0300

    HomePage RD (basic) done

--- client/src/components/Pages/HomePage/HomePage.js
+++ client/src/components/Pages/HomePage/HomePage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './HomePage.css';
 
 function HomePage() {
   return(<>
-    <h1 style={{color: "white"}}>Holaaaa</h1>
-    <img src="/images/logo.jpg" alt="logo"></img>
+    <div className="home_page_presentation">
+      <img className="left bottom" src="/icons/media.icon.svg" alt="media"></img>
+      <img className="right bottom" src="/icons/control.icon.svg" alt="control"></img>
+      <img className="left top" src="/icons/tools.icon.svg" alt="tools"></img>
+      <img className="right top" src="/icons/draw.icon.svg" alt="draw"></img>
+      <img id="logo" src="/icons/OceanStore.icon.svg" alt="OceanStore"></img>
+      <a href="/store" className="buttom">Explore</a>
+    </div>
   </>);
 }
 
